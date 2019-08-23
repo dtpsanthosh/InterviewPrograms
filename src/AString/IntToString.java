7ad853e1c69ce2d15e2908f8a08769df75ee6f1c
dtpsa <dtpsa@GSK-PC>
2019-08-23 13:22:10 +0530

    Reverse words commit

---
+++ src/AString/IntToString.java
@@ -0,0 +1,12 @@
+package AString;
+class IntToString
+{ 
+public static void main(String args[]) 
+{ 
+	int a = 1234; 
+	int b = -1234; 
+	//String str1 = Integer.toString(a); 
+	String str1 =String.valueOf(a);
+	System.out.println("String str1 = " + str1); 
+} 
+} 
